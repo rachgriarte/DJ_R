07356ecc9230c503b09943946ddd154b7b234e35
Daniel <dskay3@gmail.com>
2017-09-13 23:24:40 -0400

    heroku

--- server.js
+++ server.js
@@ -72,6 +72,28 @@ var authRoute = require('./app/routes/auth.js')(app, passport);
 // Load passport strategies
 require('./app/config/passport/passport.js')(passport, models.User);
 
+if (sequelize)
+  {
+      sequelize.authenticate().then( function() {
+          var config = sequelize.connectionManager.config;
+          console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+          
+          sequelize.query('SELECT 1+1 as test').then( function(res) {
+              
+              console.log('1+1='+res[0].test);
+              
+          });
+          
+      }).catch( function(err) {
+          var config = sequelize.connectionManager.config;
+          console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+      });
+  }
+else
+  {
+      console.log('No environnement variable found.');
+  }
+
 //Sync Database
 models.sequelize.sync({ force: true }).then(() => {
   console.log('Database is synced.')
