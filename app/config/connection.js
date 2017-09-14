ebcaf6347aed01012a2fa4f59977c3a2bb3fded6
Daniel <dskay3@gmail.com>
2017-09-13 23:28:51 -0400

    revert

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,17 +1,17 @@
-// const mysql = require('mysql');
-// var connection;
+const mysql = require('mysql');
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Kihyun123',
-//     database: 'better_db'
-//   });
-// };
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Kihyun123',
+    database: 'better_db'
+  });
+};
 
-// connection.connect();
+connection.connect();
 
-// module.exports = connection;
+module.exports = connection;
