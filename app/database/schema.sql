53eeacea34cce74b957f9ff05ea1493d422dfad0
Rach Griarte <rachelgriarte@hotmail.com>
2017-09-14 17:54:20 -0400

    deleted database folder

--- app/database/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS `better_db`;
-CREATE DATABASE `better_db`;
--- app/database/seeds.sql
+++
@@ -1,9 +0,0 @@
--- Insert preliminary data
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Big Mac", true);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Whopper", false);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Chic-fila", true);
