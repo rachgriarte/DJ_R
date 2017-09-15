ea4bb3c9d1d245e281e2431502df39858617c34a
Rach Griarte <rachelgriarte@hotmail.com>
2017-09-15 13:41:12 -0400

    fixed router

--- app/routes/account-route.js
+++ app/routes/account-route.js
@@ -5,18 +5,8 @@ const accountController = require('../controllers/account-controller.js');
 module.exports = (app) => {
   app.get('/', accountController.dashboard);
 
-  app.post('/', 
-    passport.authenticate('local-signin', {
-      // Account balance starts with 5000 and everytime player hits bet amount, it subracts
-
-      // If player wins. console.log("You won #betAmount");
-     
-    //  else if player loses . console.log (account balance - bet amount). console.log("you lost #betAmount dollars!")
-     
-      successRedirect: '/',
-
-      failureRedirect: '/',
-      failureFlash: true,
-      successMessage: 'Winner! Let Us Play Again!',
-    }));
+  app.post("/", (req, res) => {
+    console.log(req.body);
+    res.json({req.body});
+  });
 }
