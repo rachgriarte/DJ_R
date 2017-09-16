0425604f3823b4f33b7c3fb88cbf0fd3521fd3a3
Rach Griarte <rachelgriarte@hotmail.com>
2017-09-15 22:10:56 -0400

    test.js setup

--- test/test.js
+++ test/test.js
@@ -3,16 +3,16 @@
 var Nightmare = require("nightmare");
 var expect = require("chai").expect;
 
-describe("Codecademy", function() {
+describe("BETter", function() {
   // The default tests in mocha is 2 seconds.
   // Extending it to 30 seconds to have time to load the pages
-  var login = "#header__sign-in";
+  var login = "#nav-link";
 
   this.timeout(30000);
   it("should require me to login", function(done) {
     // ID for the login button.
-    Nightmare({ show: true })
-      .goto("https://codecademy.com")
+    Nightmare({ show: false })
+      .goto('http://localhost:3000/')
       // Just to be safe.
       .wait(login)
       // Click the login button.
@@ -23,14 +23,14 @@ describe("Codecademy", function() {
       })
       // Asset the title is as expected
       .then(function(title) {
-        expect(title).to.equal("Log in | Codecademy");
+        expect(title).to.equal("BETter");
         done();
       });
   });
 
-  it("should present a link to course catalog after login", function(done) {
-    Nightmare({ show: true })
-      .goto("https://codecademy.com")
+  it("should log me in", function(done) {
+    Nightmare({ show: false })
+      .goto('http://localhost:3000/')
       // Just to be safe.
       .wait(login)
       // Click the login button.
@@ -38,16 +38,16 @@ describe("Codecademy", function() {
       // Wait for the login input
       .wait("#user_login")
       // Actually log in
-      .type("#user_login", "ResilD")
-      .type("#user_password", "dummy*password")
-      .click("#user_submit")
+      .type("#user_login", "email")
+      .type("#user_password", "password")
+      .click("userPage")
       // Evaluate the following selector
       .evaluate(function() {
         // Assert the catalog exists
-        return document.querySelector("a[href='/learn/all']");
+        return document.URL
       })
       .then(function(catalog) {
-        expect(catalog).to.not.equal(undefined);
+        expect(catalog).to.not.equal(userPage);
         done();
       });
   });
